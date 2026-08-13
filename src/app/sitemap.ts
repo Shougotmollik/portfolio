92674ce41b021a6ce72059eaa28e2f04e2470222
shougot mollik <shougotmollik@gmail.com>
2026-08-13 22:54:05 +0600

    setup automatic GitHub Pages deployment

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,11 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://shougot.dev",
+      url: "https://shougotmollik.github.io",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
